db962ec59bb1e582df5a5044a5dfafa68ad0b01f
NicoAmont <86651298+NicoAmont@users.noreply.github.com>
2022-01-21 10:16:17 -0500

    Add files via upload

---
+++ pages/Landing.js
@@ -0,0 +1,15 @@
+import Date1 from "../components/Landing/Date1";
+import Hero from "../components/Landing/Hero";
+import History from "../components/Landing/History";
+
+
+export default function Landing() {
+    return (
+        <div className="bg-bg overflow-hidden">
+            
+            <Hero/>
+            
+            <History/>
+        </div>
+    )
+}
